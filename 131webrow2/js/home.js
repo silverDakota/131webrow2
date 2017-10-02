fca604dec997f6e16196d57e5b5926b94ae53f88
Unknown <swilsondev@outlook.com>
2017-10-01 22:27:49 -0500

    Created social bar.
    Created generic social bar, with JavaScript show/hide buttons, and added it to the Home Page.

--- 131webrow2/js/home.js
+++ 131webrow2/js/home.js
@@ -12,3 +12,66 @@ function tickUsers() {
     users += 1;
     document.getElementById("home-user-count").innerHTML = users.toLocaleString();
 }
+
+//For the social bar
+var socialBarImages = ["images/YouTube.png", "images/Twitter.png", "images/AppStore.png"];
+var socialBarUrls = ["https://www.youtube.com", "https://www.twitter.com", "https://itunes.apple.com/us/genre/ios/id36?mt=8"];
+
+window.addEventListener("load", SetupSocialBar, false);
+
+var bar;
+var icons = [];
+var hide;
+var show;
+
+function SetupSocialBar() {
+	bar = document.getElementById("social-bar");
+	
+	//Create social icons
+	for (i = 0; i < socialBarImages.length; i++) {
+		newLink = document.createElement("a");
+		newLink.href = socialBarUrls[i];
+		newLink.target = "_blank";
+	
+		newIcon = document.createElement("img");
+		newIcon.src = socialBarImages[i];
+		newIcon.classList.add("social-bar-icon");
+		
+		newLink.appendChild(newIcon);
+		bar.appendChild(newLink);
+		
+		icons.push(newIcon);
+	}
+	
+	//Create hide button
+	hide = document.createElement("button");
+	hide.setAttribute("id", "social-bar-hide");
+	hide.innerHTML = "hide";
+	hide.addEventListener("click", HideSocialBar, false);
+	bar.appendChild(hide);
+	
+	//Create show button
+	show = document.createElement("button");
+	show.setAttribute("id", "social-bar-show");
+	show.innerHTML = "show";
+	show.addEventListener("click", ShowSocialBar, false);
+	bar.appendChild(show);
+}
+
+function HideSocialBar() {
+	show.style.display = "block";
+	hide.style.display = "none";
+
+	for (i = 0; i < icons.length; i++) {
+		icons[i].style.display = "none";
+	}
+}
+
+function ShowSocialBar() {
+	hide.style.display = "block";
+	show.style.display = "none";
+	
+	for (i = 0; i < icons.length; i++) {
+		icons[i].style.display = "block";
+	}
+}
